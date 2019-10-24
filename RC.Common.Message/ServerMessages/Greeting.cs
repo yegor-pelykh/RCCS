6e57a1cac9a7d24376c130b74e67035c793b322d
Yegor Pelykh <yegor.dev@gmail.com>
2019-10-24 21:31:57 +0300

    Server greeting message update

--- RC.Common.Message/ServerMessages/Greeting.cs
+++ RC.Common.Message/ServerMessages/Greeting.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Net;
 
 namespace RC.Common.Message.ServerMessages
 {
@@ -6,14 +7,29 @@ namespace RC.Common.Message.ServerMessages
     {
         public override ServerMessageType MessageType { get; } = ServerMessageType.Greeting;
 
+        #region Properties
+
+        public IPAddress Ip { get; set; }
+
+        #endregion
+
         #region Overriden Methods
 
         public override void Serialize(BinaryWriter writer)
         {
+            var bytes = Ip.GetAddressBytes();
+            var length = (byte) bytes.Length;
+
+            writer.Write(length);
+            writer.Write(bytes);
         }
 
         public override void Deserialize(BinaryReader reader)
         {
+            var length = reader.ReadByte();
+            var bytes = reader.ReadBytes(length);
+
+            Ip = new IPAddress(bytes);
         }
 
         #endregion
