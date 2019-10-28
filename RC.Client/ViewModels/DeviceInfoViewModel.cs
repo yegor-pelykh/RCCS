284d17560aa740273b3505ef3d951d4a0e6d4dbe
Yegor Pelykh <yegor.dev@gmail.com>
2019-10-28 23:26:29 +0200

    MachineInfoViewModel renamed to DeviceInfoViewModel

---
+++ RC.Client/ViewModels/DeviceInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace RC.Client.ViewModels
+{
+    public class DeviceInfoViewModel : ViewModelBase
+    {
+        public DeviceInfoViewModel()
+        {
+            InstanceId = Application.Storage.Machine.InstanceId;
+            IpAddress = Application.Storage.Machine.LastKnownIpAddress;
+        }
+
+        #region Properties
+
+        public Guid InstanceId { get; private set; }
+
+        public IPAddress IpAddress { get; private set; }
+
+        #endregion
+
+    }
+
+}
