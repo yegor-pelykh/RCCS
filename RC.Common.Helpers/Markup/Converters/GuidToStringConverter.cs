d7fbf7a56ffcd072cf671b09caa7fce5b4b82c4e
Yegor Pelykh <yegor.dev@gmail.com>
2019-10-28 23:25:52 +0200

    GuidToStringConverter and IPAddressToStringConverter added

---
+++ RC.Common.Helpers/Markup/Converters/GuidToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace RC.Common.Helpers.Markup.Converters
+{
+    public class GuidToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Guid guid))
+                return null;
+
+            return guid.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string str))
+                return null;
+
+            if (!Guid.TryParse(str, out var guid))
+                return null;
+
+            return guid;
+        }
+
+    }
+
+}
---
+++ RC.Common.Helpers/Markup/Converters/IPAddressToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using Avalonia.Data.Converters;
+
+namespace RC.Common.Helpers.Markup.Converters
+{
+    public class IPAddressToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is IPAddress ip))
+                return null;
+
+            return ip.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string str))
+                return null;
+
+            if (!IPAddress.TryParse(str, out var address))
+                return null;
+
+            return address;
+        }
+
+    }
+
+}
