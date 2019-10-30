6e8ac654113439f4c8db62029e0583354762ee45
Yegor Pelykh <yegor.dev@gmail.com>
2019-10-30 23:06:03 +0200

    Create IsGreaterThanConverter.cs

---
+++ RC.Common.Helpers/Markup/Converters/IsGreaterThanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace RC.Common.Helpers.Markup.Converters
+{
+    public class IsGreaterThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(parameter is double param))
+                return false;
+
+            return (value is int vi && vi > param) || (value is double vd && vd > param);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
