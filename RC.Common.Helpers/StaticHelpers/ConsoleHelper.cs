67255e1dda07ec798e59bcd6727e7a8e2e400917
Yegor Pelykh <yegor.dev@gmail.com>
2019-10-31 19:42:03 +0200

    ConsoleHelper added

---
+++ RC.Common.Helpers/StaticHelpers/ConsoleHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RC.Common.Helpers.StaticHelpers
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintException(Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e.Message);
+            if (e.InnerException != null)
+                Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+        }
+
+    }
+
+}
